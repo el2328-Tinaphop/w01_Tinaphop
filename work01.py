e9bc05cf1378e89f841ead923208d4ed4f98764a
Tinaphop   Lapate <el2328@signalschool.ac.th>
2026-06-22 15:01:51 +0700

    Add files via upload

---
+++ work01.py
@@ -0,0 +1,45 @@
+print("Hello World")   
+print("สวัสดี")
+text = "Hello World"
+print(text)
+num = 90
+
+print(num)
+
+num01 = 90
+num02 = 13
+
+print(num01 + num02) 
+
+#int = integer = จำนวนเต็ม
+A = 110
+B = 123
+print (A)
+print(A+B)
+
+#float = ทศนิยม
+C = 3.14
+D = 5.55
+print(C)
+print(C+D)
+
+#text = ข้อความ
+Text01 = "Hello World"
+Text02 = "สวัสดี"
+print(Text01,Text02)
+
+#boolean = True/False = จริง/เท็จ
+
+#Operator
+num01 = 50
+num02 = 40
+
+print(num01 * num02)
+
+#+
+A = complete = num01 + num02
+print(A)
+#-
+B = complete = num01 - num02 + num02
+print(B)
+print(A+B)
